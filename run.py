e31735f1785c9ee359f08ccfe9d1b5af8671f435
mristimaki <rosanna.ristimaki@gmail.com>
2023-06-28 00:20:47 +0000

    Add basic structure and created the database

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+from taskmanager import app
+from taskmanager.models import Category, Task
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
